1ba4844d03672ec5bf9cd798c4a3598376b41f55
Lorenzo <lorenzo@bitcoinquark.org>
2018-03-17 20:03:46 +0800

    GIT REPO

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -26,7 +26,7 @@ for repo in electrum electrum-locale electrum-icons; do
 	git checkout master
 	cd ..
     else
-	URL=https://github.com/spesmilo/$repo.git
+	URL=https://github.com/bitcoinquark/$repo.git
 	git clone -b master $URL $repo
     fi
 done
